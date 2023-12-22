ecabd41f2ab2b90e5f1722f022de34790301341e
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-22 21:11:57 +0700

    Add files via upload

--- rest-countries/public/main.js
+++ rest-countries/public/main.js
@@ -1,14 +1,17 @@
-const { response } = require("express");
+document.getElementById("form").addEventListener("submit", nameSubmit);
 
-async function nameSubmit() {
+async function nameSubmit(e) {
+    e.preventDefault();
     try {
         const name = document.getElementById("input").value;
 
-        response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
-        for(i=0; i<response.length; i++) {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+        const data = response.json();
+
+        for(i=0; i<data.length; i++) {
             const p = document.createElement("p");
-            p.innerHTML = response[i].name.common;
-            document.appendChild(p);
+            p.innerHTML = data[i].name.common;
+            document.body.appendChild(p);
         }
     } catch(err) {
         console.error(err);
