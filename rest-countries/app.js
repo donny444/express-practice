56547fb8370e194f8422207436f9d5f1129cf8cb
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-22 21:10:18 +0700

    Delete rest-countries/app.js

--- rest-countries/app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const app = require("./app.js");
-const express = require("express");
-
-const jj = express();
-const server = http.createServer(app);
-jj.use("/", express.static("public"));
-server.listen(8000, () => console.log("Server is running on port 8000"));
