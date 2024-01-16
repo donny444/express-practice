92e32111bcd9a4fa93c1d73319ccf60164a4b90b
Donny <tanadornrach@gmail.com>
2024-01-16 21:21:45 +0700

    try

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use(function (req, res, next) {
+    console.log('Middleware Time: %d', Date.now());
+    console.log(req.baseUrl);
+    next();
+})
 
-app.listen(port, () => {
-    console.log(`Example app listen on port ${port}`);
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+})
+app.route("/any")
+    .get(function (req, res, next) {
+        res.set("Content-Type", "text/html");
+        return res.send("Hello GET");
+    })
+    .post(function (req, res, next) {
+        res.set("Content-Type", "text/html");
+        return res.send("Hello POST");
+    })
+    .put(function (req, res, next) {
+        res.set("Content-Type", "text/html");
+        return res.send("Hello PUT");
+    })
+    .delete(function (req, res, next) {
+        res.set("Content-Type", "text/html");
+        return res.send("Hello DELETE");
+    })
+
+app.listen(3000, () => {
+    console.log(`Example app listen on port 3000`);
 })
